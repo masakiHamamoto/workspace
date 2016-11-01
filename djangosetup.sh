9cc8976ed73bcd25339c0f24d945ef4ab37718c5
masakiHamamoto <hama.makki@gmail.com>
2016-11-02 06:07:50 +0900

    test

--- djangosetup.sh
+++ djangosetup.sh
@@ -21,11 +21,11 @@ function virtualenv_install {
     sudo pip install virtualenv
 }
 
-if [ ! -z `which virtualenv` ]; then
-    virtualenv_install
-fi
+#if [ ! -z `which virtualenv` ]; then
+#    virtualenv_install
+#fi
 
-virtualenv_dep_install
+#virtualenv_dep_install
 echo "Now performing Django tasks"
 #python manage.py makemigrations
 #python manage.py migrate
