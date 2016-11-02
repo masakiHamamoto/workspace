3de74780d6230a7677e5b423c14f209707059539
masakiHamamoto <hama.makki@gmail.com>
2016-11-02 09:49:01 +0900

    test

--- djangosetup.sh
+++ djangosetup.sh
@@ -2,30 +2,35 @@
 
 echo "Now installing all Python tools"
 
-function virtualenv_dep_install {
-    cd ref_manager
-    if [ ! -d venv/bin ]; then
-	virtualenv venv 
-	echo "Created virtual environment"
-    fi
-    if [ ! -f venv/updated ]; then
-	source venv/bin/activate
-	pip install -r requirements.txt
-	touch venv/updated
-	echo "Installed python packages"
-    fi
-}
+#function virtualenv_dep_install {
+#    cd ref_manager
+#    if [ ! -d venv/bin ]; then
+#	virtualenv venv 
+#	echo "Created virtual environment"
+#    fi
+#    if [ ! -f venv/updated ]; then
+#	source venv/bin/activate
+#	pip install -r requirements.txt
+#	touch venv/updated
+#	echo "Installed python packages"
+#    fi
+#}
 
-function virtualenv_install {
-    echo "Installing virtualenv, it may ask for SUDO password"
-    sudo pip install virtualenv
-}
+#function virtualenv_install {
+#    echo "Installing virtualenv, it may ask for SUDO password"
+#    sudo pip install virtualenv
+#}
 
 #if [ ! -z `which virtualenv` ]; then
 virtualenv_install
 #fi
+cd ref_manager
+virtualenv venv 
+source venv/bin/activate
+pip install -r requirements.txt
+touch venv/updated
 
-virtualenv_dep_install
+#virtualenv_dep_install
 echo "Now performing Django tasks"
 python -V
 #python manage.py makemigrations
